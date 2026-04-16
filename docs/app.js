65c419754d47f06d1849eb2f40198c4c736699c8
luke <lwmo226@uky.edu>
2026-04-15 21:26:27 -0400

    name change

--- docs/app.js
+++ docs/app.js
@@ -9,7 +9,7 @@
 
 // ---------- Config ----------
 const OWNER = "lukeM2405";
-const REPO = "Horse-Racing-Model";
+const REPO = "horse-Racing-Model";
 const BRANCH = "main";
 
 // Directory holding the day-cards and filename pattern.
